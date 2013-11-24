6813c577428ba4efcc7031a5a6809f15439bf6d2
DjDeMi <djdemi@gmail.com>
2013-11-24 20:48:51 +0100

    Advanced Settings 2

---
+++ advanced.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python3.3
+# coding= utf-8
+from gi.repository import Gtk
+
+class AdvancedWindow(Gtk.Window):
+
+    def __init__(self, parent, device):
+        Gtk.Window.__init__(self)
+        self.device = device
+        self.parent = parent
+
+        self.set_title("Advanced Settings")
+        self.set_position(Gtk.WindowPosition.CENTER)
+        self.set_transient_for(parent)
+        self.set_modal(True)
+
+        vbox = Gtk.VBox(False)
+        vbox.set_spacing(5)
+        self.add(vbox)
+
+        hbox_design_capacity = Gtk.HBox(False)
+        vbox.pack_start(hbox_design_capacity, False, False, 0)
+
+        design_capacity_button = Gtk.Button(label="Change Design Capacity")
+        design_capacity_button.set_size_request(250, 50)
+        design_capacity_button.connect("clicked", self.on_design_capacity_button_clicked)
+        hbox_design_capacity.pack_start(design_capacity_button, False, False, 6)
+
+        hbox_end_of_discharge_voltage = Gtk.HBox(False)
+        vbox.pack_start(hbox_end_of_discharge_voltage, False, False, 0)
+
+        discharge_voltage_button = Gtk.Button(label="Change End of Discharge Voltage")
+        discharge_voltage_button.set_size_request(250, 50)
+        discharge_voltage_button.connect("clicked", self.on_discharge_voltage_button_clicked)
+        hbox_end_of_discharge_voltage.pack_start(discharge_voltage_button, False, False, 6)
+
+        hbox_device_configuration_2 = Gtk.HBox(False)
+        vbox.pack_start(hbox_device_configuration_2, False, False, 0)
+
+        device_configuration_2_button = Gtk.Button(label="Change Device configuration 2")
+        device_configuration_2_button.set_size_request(250, 50)
+        device_configuration_2_button.connect("clicked", self.on_device_configuration_button_clicked)
+        hbox_device_configuration_2.pack_start(device_configuration_2_button, False, False, 6)
+
+        hbox_display_configuration1 = Gtk.HBox(False)
+        vbox.pack_start(hbox_display_configuration1, False, False, 0)
+
+        display_configuration_1_button = Gtk.Button(label="Change Display Configuration 1")
+        display_configuration_1_button.set_size_request(250, 50)
+        display_configuration_1_button.connect("clicked", self.on_display_configuration_1_button_clicked)
+        hbox_display_configuration1.pack_start(display_configuration_1_button, False, False, 6)
+
+        hbox_display_configuration2 = Gtk.HBox(False)
+        vbox.pack_start(hbox_display_configuration2, False, False, 0)
+
+        display_configuration_2_button = Gtk.Button(label="Change Display Configuration 2")
+        display_configuration_2_button.set_size_request(250, 50)
+        display_configuration_2_button.connect("clicked", self.on_display_configuration_2_button_clicked)
+        hbox_display_configuration2.pack_start(display_configuration_2_button, False, False, 6)
+
+        hbox_display_configuration3 = Gtk.HBox(False)
+        vbox.pack_start(hbox_display_configuration3, False, False, 0)
+
+        display_configuration_3_button = Gtk.Button(label="Change Display Configuration 3")
+        display_configuration_3_button.set_size_request(250, 50)
+        display_configuration_3_button.connect("clicked", self.on_display_configuration_3_button_clicked)
+        hbox_display_configuration3.pack_start(display_configuration_3_button, False, False, 6)
+
+        hbox_display_configuration4 = Gtk.HBox(False)
+        vbox.pack_start(hbox_display_configuration4, False, False, 0)
+
+        display_configuration_4_button = Gtk.Button(label="Change Display Configuration 4")
+        display_configuration_4_button.set_size_request(250, 50)
+        display_configuration_4_button.connect("clicked", self.on_display_configuration_4_button_clicked)
+        hbox_display_configuration4.pack_start(display_configuration_4_button, False, False, 6)
+
+        hbox_display_configuration5 = Gtk.HBox(False)
+        vbox.pack_start(hbox_display_configuration5, False, False, 0)
+
+        display_configuration_5_button = Gtk.Button(label="Change Display Configuration 5")
+        display_configuration_5_button.set_size_request(250, 50)
+        display_configuration_5_button.connect("clicked", self.on_display_configuration_5_button_clicked)
+        hbox_display_configuration5.pack_start(display_configuration_5_button, False, False, 6)
+
+    def on_design_capacity_button_clicked(self, button):
+        # between 1 to 3270
+        print("Design Capacity")
+        while True:
+            value = self.get_value("Insert value between 1 to 3270", "Change design capacity")
+            print(value)
+            if(value != None):
+                if(0<int(value,10)<3271):
+                    print("Balio zuzena, ez da None")
+                    break
+                else:
+                    print("Balio desegokia")
+                    self.error_message("Data error", "Value must be between 1 to 3270. Try again.")
+            else:
+                print("None")
+                self.error_message("Data error", "You need insert a value between 1 to 3270. Try again.")
+        value = value.zfill(4)
+        print(value)
+        print(len(hex(int(value,10))[2:]))
+        hex_value = hex(int(value,10))[2:].zfill(4).upper()
+        print(hex_value)
+        convert_string = "FF172440" + hex_value[2:4] + hex_value[0:2]
+        print(convert_string)
+        hex_value_string = bytearray.fromhex(convert_string)
+        convert_string += hex(self.crc(hex_value_string))[2:].upper()
+        hex_value_string = bytearray.fromhex(convert_string)
+        print(hex_value_string)
+        self.device.send_command(hex_value_string)
+        try:
+            self.device.read_input(3)
+        except ACKError:
+            self.error_message("Data error", "Received data has not ACK")
+        except CRCError:
+            self.error_message("Data error", "Received CRC is not correct")
+        except sizeError:
+            self.error_message("Data error", "Received data has not correct size")
+        except timeoutError:
+            self.error_message("Timeout", "The device doesn't respond.")
+        else:
+            self.device.send_command(b'\xFF\x16\x24\x40\x01\x00\x73')
+            try:
+                raw_data = self.device.read_input(5)
+            except ACKError:
+                self.error_message("Data error", "Received data has not ACK")
+            except CRCError:
+                self.error_message("Data error", "Received CRC is not correct")
+            except sizeError:
+                self.error_message("Data error", "Received data has not correct size")
+            except timeoutError:
+                self.error_message("Timeout", "The device doesn't respond.")
+            else:
+                logging.debug("aaa")
+                self.update_data()
+                logging.debug("rsoc reset")
+
+    def on_discharge_voltage_button_clicked(self, button):
+        # between 4000 to 65535
+        print("Discharge Voltage")
+
+    def on_device_configuration_button_clicked(self, button):
+        # between 0000 to BFFF
+        print("Device Configuration")
+        print(int("BFFF",16))
+
+    def on_display_configuration_1_button_clicked(self, button):
+        # between 0000 to FFFF
+        print("Display Configuration 1")
+
+    def on_display_configuration_2_button_clicked(self, button):
+        # between 0000 to FFFF
+        print("Display Configuration 2")
+
+    def on_display_configuration_3_button_clicked(self, button):
+        # between 0000 to FFFF
+        print("Display Configuration 3")
+
+    def on_display_configuration_4_button_clicked(self, button):
+        # between 0000 to FFFF
+        print("Display Configuration 4")
+
+    def on_display_configuration_5_button_clicked(self, button):
+        # between 0000 to FFFF
+        print("Display Configuration 5")
+
+    def get_value(parent, message, title=''):
+        # Returns user input as a string or None
+        # If user does not input text it returns None, NOT AN EMPTY STRING.
+        dialogWindow = Gtk.MessageDialog(parent, Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT, Gtk.MessageType.QUESTION, Gtk.ButtonsType.OK_CANCEL, message)
+
+        dialogWindow.set_title(title)
+
+        dialogBox = dialogWindow.get_content_area()
+        userEntry = Gtk.Entry()
+        userEntry.set_size_request(250,0)
+        dialogBox.pack_end(userEntry, False, False, 0)
+
+        dialogWindow.show_all()
+        response = dialogWindow.run()
+        text = userEntry.get_text() 
+        dialogWindow.destroy()
+        if (response == Gtk.ResponseType.OK) and (text != ''):
+            return text
+        else:
+            return None
+
+    def error_message(self, firstMessage, secondMessage):
+        dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.ERROR, Gtk.ButtonsType.OK, str(firstMessage))
+        dialog.format_secondary_text(str(secondMessage))
+        dialog.run()
+        dialog.destroy()
+
+    def HexToByte(self, hexStr):
+        """
+        Convert a string hex byte values into a byte string. The Hex Byte values may
+        or may not be space separated.
+        """
+        # The list comprehension implementation is fractionally slower in this case    
+        #
+        #    hexStr = ''.join( hexStr.split(" ") )
+        #    return ''.join( ["%c" % chr( int ( hexStr[i:i+2],16 ) ) \
+        #                                   for i in range(0, len( hexStr ), 2) ] )
+     
+        bytes = []
+
+        hexStr = ''.join( hexStr.split(" ") )
+
+        for i in range(0, len(hexStr), 2):
+            bytes.append( chr( int (hexStr[i:i+2], 16 ) ) )
+
+        return ''.join( bytes )
+
+    def crc(self, data):
+        result = 0
+        for c in data[1:]:
+            result = result ^ c
+        return result
---
+++ main2.py
@@ -0,0 +1,272 @@
+#!/usr/bin/env python3.3
+# coding= utf-8
+from gi.repository import Gtk, GObject, AppIndicator3
+from bq78412 import Device
+from arguments import *
+from exceptions import *
+import logging
+from advanced import AdvancedWindow
+
+class MainWindow(Gtk.Window):
+
+    def __init__(self, device):
+        Gtk.Window.__init__(self)
+        self.device = device
+
+        self.ind = AppIndicator3.Indicator.new("example-simple-client", "indicator-messages", AppIndicator3.IndicatorCategory.APPLICATION_STATUS)
+        self.ind.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
+        self.ind.set_attention_icon ("indicator-messages-new")
+        # Icono para el indicador
+        self.ind.set_icon("distributor-logo")
+
+        #new menu
+        self.menu = Gtk.Menu()
+
+        self.menuItemData = Gtk.MenuItem("Get Data")
+        self.menuItemData.connect("activate", self.on_read_button_clicked)
+        self.menu.append(self.menuItemData)
+
+        self.menuItemRsoc = Gtk.MenuItem("Reset RSOC")
+        self.menuItemRsoc.connect("activate", self.on_rsoc_button_clicked)
+        self.menu.append(self.menuItemRsoc)
+
+        self.menuItemAdvanced = Gtk.MenuItem("Advanced Settings")
+        self.menuItemAdvanced.connect("activate", self.on_advanced_button_clicked)
+        self.menu.append(self.menuItemAdvanced)
+
+        self.menuCheckData = Gtk.CheckMenuItem("Auto Refresh")
+        self.menuCheckData.connect("activate", self.on_refresh_toggled)
+        self.menu.append(self.menuCheckData)
+
+        self.menuItemExit = Gtk.MenuItem("Exit")
+        self.menuItemExit.connect("activate", Gtk.main_quit)
+        self.menu.append(self.menuItemExit)
+
+        self.menuItemData.show()
+        self.menuItemRsoc.show()
+        self.menuItemAdvanced.show()
+        self.menuCheckData.show()
+        self.menuItemExit.show()
+
+        self.ind.set_menu(self.menu)
+
+
+        self.set_title("bq78412")
+        self.set_position(Gtk.WindowPosition.CENTER)
+
+        vbox = Gtk.VBox(False)
+        self.add(vbox)
+
+        # voltage hbox
+        hbox_voltage = Gtk.HBox(False)
+        vbox.pack_start(hbox_voltage, False, False, 0)
+
+        voltage_label = Gtk.Label("Voltage")
+        voltage_label.set_size_request(100, 50)
+        hbox_voltage.pack_start(voltage_label, False, False, 6)
+
+        self.voltage_value = Gtk.Label(0)
+        self.voltage_value.set_size_request(100, 50)
+        hbox_voltage.pack_start(self.voltage_value, False, False, 6)
+
+        voltage_unit = Gtk.Label("mV")
+        voltage_unit.set_size_request(100, 50)
+        hbox_voltage.pack_start(voltage_unit, False, False, 6)
+
+        # current hbox
+        hbox_current = Gtk.HBox(False)
+        vbox.pack_start(hbox_current, False, False, 0)
+
+        current_label = Gtk.Label("Current")
+        current_label.set_size_request(100, 50)
+        hbox_current.pack_start(current_label, False, False, 6)
+
+        self.current_value = Gtk.Label(0)
+        self.current_value.set_size_request(100, 50)
+        hbox_current.pack_start(self.current_value, False, False, 6)
+
+        current_unit = Gtk.Label("mA")
+        current_unit.set_size_request(100, 50)
+        hbox_current.pack_start(current_unit, False, False, 6)
+
+        # avg_current hbox
+        hbox_avg_current = Gtk.HBox(False)
+        vbox.pack_start(hbox_avg_current, False, False, 0)
+
+        avg_current_label = Gtk.Label("Avg. current")
+        avg_current_label.set_size_request(100, 50)
+        hbox_avg_current.pack_start(avg_current_label, False, False, 6)
+
+        self.avg_current_value = Gtk.Label(0)
+        self.avg_current_value.set_size_request(100, 50)
+        hbox_avg_current.pack_start(self.avg_current_value, False, False, 6)
+
+        avg_current_unit = Gtk.Label("mA")
+        avg_current_unit.set_size_request(100, 50)
+        hbox_avg_current.pack_start(avg_current_unit, False, False, 6)
+
+        # temperature hbox
+        hbox_temperature = Gtk.HBox(False)
+        vbox.pack_start(hbox_temperature, False, False, 0)
+
+        temperature_label = Gtk.Label("Temperature")
+        temperature_label.set_size_request(100, 50)
+        hbox_temperature.pack_start(temperature_label, False, False, 6)
+
+        self.temperature_value = Gtk.Label(0)
+        self.temperature_value.set_size_request(100, 50)
+        hbox_temperature.pack_start(self.temperature_value, False, False, 6)
+
+        temperature_unit = Gtk.Label("ºC")
+        temperature_unit.set_size_request(100, 50)
+        hbox_temperature.pack_start(temperature_unit, False, False, 6)
+
+        # rsoc hbox
+        hbox_rsoc = Gtk.HBox(False)
+        vbox.pack_start(hbox_rsoc, False, False, 0)
+
+        rsoc_label = Gtk.Label("RSOC")
+        rsoc_label.set_size_request(100, 50)
+        hbox_rsoc.pack_start(rsoc_label, False, False, 6)
+
+        self.rsoc_value = Gtk.Label(0)
+        self.rsoc_value.set_size_request(100, 50)
+        hbox_rsoc.pack_start(self.rsoc_value, False, False, 6)
+
+        rsoc_unit = Gtk.Label("%")
+        rsoc_unit.set_size_request(100, 50)
+        hbox_rsoc.pack_start(rsoc_unit, False, False, 6)
+
+        # refresh hbox
+        hbox_refresh = Gtk.HBox(False)
+        vbox.pack_start(hbox_refresh, False, False, 6)
+
+        adj = Gtk.Adjustment(0.5, 0.5, 60, 0.5, 1, 0)
+        self.spinner = Gtk.SpinButton()
+        self.spinner.configure(adj, 0, 0)
+        self.spinner.set_digits(1)
+        hbox_refresh.pack_start(self.spinner, False, False, 6)
+
+        spinner_label = Gtk.Label("in minutes")
+        spinner_label.set_size_request(100, 50)
+        hbox_refresh.pack_start(spinner_label, False, False, 6)
+
+        self.refresh_check = Gtk.CheckButton("Auto refresh")
+        self.refresh_check.set_size_request(100, 50)
+        self.refresh_check.connect("toggled", self.on_refresh_toggled)
+        hbox_refresh.pack_start(self.refresh_check, False, False, 6)
+
+        # buttons hbox
+        hbox_buttons = Gtk.HBox(False)
+        vbox.pack_start(hbox_buttons, False, False, 6)
+
+        read_button = Gtk.Button(label="Read data")
+        read_button.set_size_request(156, 50)
+        read_button.connect("clicked", self.on_read_button_clicked)
+        hbox_buttons.pack_start(read_button, False, False, 6)
+
+        rsoc_button = Gtk.Button(label="RSOC 100%")
+        rsoc_button.set_size_request(156, 50)
+        rsoc_button.connect("clicked", self.on_rsoc_button_clicked)
+        hbox_buttons.pack_start(rsoc_button, False, False, 6)
+
+
+        # advanced setting button
+        hbox_advanced = Gtk.HBox(False)
+        vbox.pack_start(hbox_advanced, False, False, 6)
+
+        advanced_button = Gtk.Button(label="Advanced Settings")
+        advanced_button.set_size_request(312, 50)
+        advanced_button.connect("clicked", self.on_advanced_button_clicked)
+        hbox_advanced.pack_start(advanced_button, False, False, 6)
+
+        # attributes
+        self.refresh = False
+
+    def on_read_button_clicked(self, button):
+        self.update_data()
+
+    def on_rsoc_button_clicked(self, button):
+        try:
+            self.device.reset_rsoc()
+        except ACKError:
+            self.error_message("Data error", "Received data has not ACK")
+        except CRCError:
+            self.error_message("Data error", "Received CRC is not correct")
+        except sizeError:
+            self.error_message("Data error", "Received data has not correct size")
+        except timeoutError:
+            self.error_message("Timeout", "The device doesn't respond.")
+        else:
+            logging.debug("aaa")
+            self.update_data()
+            logging.debug("rsoc reset")
+
+    def on_refresh_toggled(self, refresh_check):
+        self.refresh = refresh_check.get_active()
+        logging.debug("Data update time: " + str(self.spinner.get_value()))
+        if self.refresh:
+            GObject.timeout_add(self.spinner.get_value()*60*1000, self.refresh_data())
+
+    def refresh_data(self):
+        if self.refresh:
+            self.update_data()
+        return self.refresh
+    
+    def error_message(self, firstMessage, secondMessage):
+        dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.ERROR, Gtk.ButtonsType.OK, str(firstMessage))
+        dialog.format_secondary_text(str(secondMessage))
+        dialog.run()
+        logging.debug("ERROR dialog closed")
+        dialog.destroy()
+
+    def update_data(self):
+        try:
+            logging.debug("Refresh value check " + str(self.refresh_check))
+            logging.debug("Refresh value " + str(self.refresh))
+            data = self.get_data()
+        except ACKError:
+            self.error_message("Data error", "Received data has not ACK")
+            self.refresh_check.set_active(False)
+            self.refresh = False
+        except CRCError:
+            self.error_message("Data error", "Received CRC is not correct")
+            self.refresh_check.set_active(False)
+            self.refresh = False
+        except sizeError:
+            self.error_message("Data error", "Received data has not correct size")
+            self.refresh_check.set_active(False)
+            self.refresh = False
+        except timeoutError:
+            self.error_message("Timeout", "The device doesn't respond.")
+            self.refresh_check.set_active(False)
+            self.refresh = False
+            logging.debug("Refresh value check" + str(self.refresh_check))
+            logging.debug("Refresh value " + str(self.refresh))
+        else:
+            self.voltage_value.set_text(str(data['voltage']))
+            self.current_value.set_text(str(data['current']))
+            self.avg_current_value.set_text(str(data['avg_current']))
+            self.temperature_value.set_text(str(data['temperature']))
+            self.rsoc_value.set_text(str(data['rsoc']))
+            logging.debug("data updated with: "+str(data))
+            
+
+    def get_data(self):
+        return self.device.get_data()
+
+    def on_advanced_button_clicked(self, button):
+        adWindow = AdvancedWindow(self, self.device)
+        adWindow.show_all()
+
+args = read_arguments()
+if args['verbose']:
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+    
+logging.debug("Received args: "+ str(args))
+logging.debug("Trying to connect")
+device = Device(args['port'], args['timeout'], 9600)
+win = MainWindow(device)
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
